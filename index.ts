ebe79f4e861dd0dd499ac6b554e0f940ef68855c
Zahira Khan <66169164+zahirakhan@users.noreply.github.com>
2024-03-16 19:55:27 -0700

    Add files via upload
    simple calculator y using TS - NodeJS - INQUIRER

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstnumber" },
+  { message: "enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "select one of these operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+
+//conditional statement
+if (answer.operator === "addition") {
+  console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstnumber / answer.secondnumber);
+} else {
+  console.log("Please select a valid operator");
+}
